f3dccf72e38f38b8e31d2db6bd1b6c70b07a020a
Vikram Palnati <vikram.palnati@caccord.com>
2021-12-22 12:00:09 +0530

    removed some files

--- student_app/forms.py
+++
@@ -1,7 +0,0 @@
-from student_app.models import *
-from django import forms 
-
-class StudentDetailsForm(forms.ModelForm):
-    class Meta:
-        model = StudentDetails
-        fields = "__all__"
